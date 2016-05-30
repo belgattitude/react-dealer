ab2cdad6b717becfac5903d3f15a734c837c30d8
belgattitude <belgattitude@gmail.com>
2016-05-30 19:49:58 +0200

    Playground updated for dealers

--- src/js/dealer/dealer_service.js
+++ src/js/dealer/dealer_service.js
@@ -1,14 +1,20 @@
 import 'whatwg-fetch';
 
 class DealerService {
+
     constructor(options) {
         this.options = options;
     }
 
     findDealers() {
-        return fetch('/dealers.json');
-        fetch('/dealers.json')
-            .then(function (response) {
+
+        var url = 'http://localhost/emdmusic_server/public/api/v1/dealer';
+
+        fetch(url, {
+           // mode: 'no-cors',
+           // credentials: 'same-origin'
+        }).then(function (response) {
+            console.log('response', response);
                 return response.json()
             }).then(function (json) {
             console.log('parsed json', json)
